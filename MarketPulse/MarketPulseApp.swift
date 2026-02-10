6e9f9a0e0428bc5edc3736aa248646dde7eb6047
matvejpetrusov <matvey.flame@gmail.com>
2026-02-10 13:45:38 +0300

    Initial Commit

---
+++ MarketPulse/MarketPulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarketPulseApp.swift
+//  MarketPulse
+//
+//  Created by Матвей Петрушов on 10.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct MarketPulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
